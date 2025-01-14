9d5138adf15ef87ac660ab0709dd554be28f365a
StrayBeing <maciej.sobierajski.zsti@gmail.com>
2025-01-14 10:40:45 +0100

    Fixes

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import styles from './page.module.css';
 import Link from 'next/link';
-import ImageSlideshow from './/components/images/image-slideshow';
+import ImageSlideshow from './components/images/image-slideshow';
+
 export default function Home() {
   return (
     <main>
@@ -10,8 +11,12 @@ export default function Home() {
           <h1>NextLevel Food for NextLevel Foodies!</h1>
           <p>Taste & share food from all over the world.</p>
           <div className={styles.cta}>
-            <a href="/community">Join the Community</a>
-            <a href="/meals">Explore Meals</a>
+            <Link href="/community" passHref>
+              <a>Join the Community</a>
+            </Link>
+            <Link href="/meals" passHref>
+              <a>Explore Meals</a>
+            </Link>
           </div>
         </div>
       </header>
